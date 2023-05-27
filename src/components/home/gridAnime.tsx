d4428b1926a42584650f32e2f41a5e5c826e8326
yrllanbrandao <yrllanbrandao@outlook.com>
2023-05-27 19:24:54 -0300

    recents anime layout

--- src/components/home/gridAnime.tsx
+++ src/components/home/gridAnime.tsx
@@ -6,28 +6,166 @@ import { Link } from "react-router-dom";
 const RecentAnimes = () => {
 
     
-    return(<>
-        <ul className="grid-anime-home">
-            <Link className="grid-anime-item-home" to={"/home"}></Link>
-            <Link className="grid-anime-item-home" to={"/"}></Link>
-            <Link className="grid-anime-item-home" to={"/"}></Link>
-            <Link className="grid-anime-item-home" to={"/"}></Link>
-            <Link className="grid-anime-item-home" to={"/"}></Link>
-            <Link className="grid-anime-item-home" to={"/"}></Link>
-            <Link className="grid-anime-item-home" to={"/"}></Link>
-            <Link className="grid-anime-item-home" to={"/"}></Link>
-            <Link className="grid-anime-item-home" to={"/"}></Link>
-            <Link className="grid-anime-item-home" to={"/"}></Link>
-            <Link className="grid-anime-item-home" to={"/"}></Link>
-            <Link className="grid-anime-item-home" to={"/"}></Link>
-            <Link className="grid-anime-item-home" to={"/"}></Link>
-            <Link className="grid-anime-item-home" to={"/"}></Link>
-            <Link className="grid-anime-item-home" to={"/"}></Link>
-            <Link className="grid-anime-item-home mobile-only" to={"/"}></Link>
-            
-            {/*  */}
+    return(<div className="recents-container">
+         <ul className="recents d-flex flex-column  gap-1">
+        <li className="recents-row container-fluid d-flex">
+        <Link to={"/"} className="recents-item">
+            <div className="recents-item-thumbnail">
+
+            </div>
+            <h2 className="fs-6 text-center">anime name</h2>
+            <span>episode x</span>
+        </Link>
+        <Link to={"/"} className="recents-item">
+            <div className="recents-item-thumbnail">
+
+            </div>
+            <h2 className="fs-6 text-center">anime name</h2>
+            <span>episode x</span>
+        </Link>
+        <Link to={"/"} className="recents-item">
+            <div className="recents-item-thumbnail">
+
+            </div>
+            <h2 className="fs-6 text-center">anime name</h2>
+            <span>episode x</span>
+        </Link>
+        <Link to={"/"} className="recents-item">
+            <div className="recents-item-thumbnail">
+
+            </div>
+            <h2 className="fs-6 text-center">anime name</h2>
+            <span>episode x</span>
+        </Link>
+        <Link to={"/"} className="recents-item">
+            <div className="recents-item-thumbnail">
+
+            </div>
+            <h2 className="fs-6 text-center">anime name</h2>
+            <span>episode x</span>
+        </Link>
+       
+         </li>
+         {/*  */}
+         <li className="recents-row container-fluid d-flex">
+        <Link to={"/"} className="recents-item">
+            <div className="recents-item-thumbnail">
+
+            </div>
+            <h2 className="fs-6 text-center">anime name</h2>
+            <span>episode x</span>
+        </Link>
+        <Link to={"/"} className="recents-item">
+            <div className="recents-item-thumbnail">
+
+            </div>
+            <h2 className="fs-6 text-center">anime name</h2>
+            <span>episode x</span>
+        </Link>
+        <Link to={"/"} className="recents-item">
+            <div className="recents-item-thumbnail">
+
+            </div>
+            <h2 className="fs-6 text-center">anime name</h2>
+            <span>episode x</span>
+        </Link>
+        <Link to={"/"} className="recents-item">
+            <div className="recents-item-thumbnail">
+
+            </div>
+            <h2 className="fs-6 text-center">anime name</h2>
+            <span>episode x</span>
+        </Link>
+        <Link to={"/"} className="recents-item">
+            <div className="recents-item-thumbnail">
+
+            </div>
+            <h2 className="fs-6 text-center">anime name</h2>
+            <span>episode x</span>
+        </Link>
+       
+         </li>
+         {/*  */}
+         <li className="recents-row container-fluid d-flex">
+        <Link to={"/"} className="recents-item">
+            <div className="recents-item-thumbnail">
+
+            </div>
+            <h2 className="fs-6 text-center">anime name</h2>
+            <span>episode x</span>
+        </Link>
+        <Link to={"/"} className="recents-item">
+            <div className="recents-item-thumbnail">
+
+            </div>
+            <h2 className="fs-6 text-center">anime name</h2>
+            <span>episode x</span>
+        </Link>
+        <Link to={"/"} className="recents-item">
+            <div className="recents-item-thumbnail">
+
+            </div>
+            <h2 className="fs-6 text-center">anime name</h2>
+            <span>episode x</span>
+        </Link>
+        <Link to={"/"} className="recents-item">
+            <div className="recents-item-thumbnail">
+
+            </div>
+            <h2 className="fs-6 text-center">anime name</h2>
+            <span>episode x</span>
+        </Link>
+        <Link to={"/"} className="recents-item">
+            <div className="recents-item-thumbnail">
+
+            </div>
+            <h2 className="fs-6 text-center">anime name</h2>
+            <span>episode x</span>
+        </Link>
+       
+         </li>
+     {/*  */}
+     <li className="recents-row container-fluid d-flex">
+        <Link to={"/"} className="recents-item">
+            <div className="recents-item-thumbnail">
+
+            </div>
+            <h2 className="fs-6 text-center">anime name</h2>
+            <span>episode x</span>
+        </Link>
+        <Link to={"/"} className="recents-item">
+            <div className="recents-item-thumbnail">
+
+            </div>
+            <h2 className="fs-6 text-center">anime name</h2>
+            <span>episode x</span>
+        </Link>
+        <Link to={"/"} className="recents-item">
+            <div className="recents-item-thumbnail">
+
+            </div>
+            <h2 className="fs-6 text-center">anime name</h2>
+            <span>episode x</span>
+        </Link>
+        <Link to={"/"} className="recents-item">
+            <div className="recents-item-thumbnail">
+
+            </div>
+            <h2 className="fs-6 text-center">anime name</h2>
+            <span>episode x</span>
+        </Link>
+        <Link to={"/"} className="recents-item">
+            <div className="recents-item-thumbnail">
+
+            </div>
+            <h2 className="fs-6 text-center">anime name</h2>
+            <span>episode x</span>
+        </Link>
+       
+         </li>
         </ul>
-    </>)
+    
+    </div>)
 }
 
 export default RecentAnimes;
